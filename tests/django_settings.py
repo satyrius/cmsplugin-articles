45f8e0fe00a168c756a3e18b56811238b4cc2193
Anton Egorov <anton.egoroff@gmail.com>
2014-11-14 03:03:49 +0300

    test app with tox

---
+++ tests/django_settings.py
@@ -0,0 +1,29 @@
+LANGUAGE_CODE = 'en'
+SECRET_KEY = 'ji2r2iGkZqJVbWDhXrgDKDR2qG#mmtvBZXPXDugA4H)KFLwLHy'
+SITE_ID = 1
+
+SOUTH_TESTS_MIGRATE = True
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+NOSE_ARGS = ['--nologcapture']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sites',
+    'django_nose',
+    'cms',
+    'mptt',
+    'south',
+    'cmsplugin_articles',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.core.context_processors.request',
+]

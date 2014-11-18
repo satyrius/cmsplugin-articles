d2513ba01bfb0a1b1e5ba52e57eb00e3c8b828f8
Anton Egorov <anton.egoroff@gmail.com>
2014-11-19 01:59:18 +0300

    published_at template tag

--- cmsplugin_articles/templatetags/article_tags.py
+++ cmsplugin_articles/templatetags/article_tags.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter()
 def article_title(article_page):
     return article_page.get_title()
+
+
+@register.filter()
+def published_at(article_page):
+    return article_page.publication_date or article_page.creation_date
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,6 +1,7 @@
 from cms.api import create_page
 from django.template import Template, Context
 from django.test import TestCase
+from freezegun import freeze_time
 
 
 class TemplatetagsTest(TestCase):
@@ -21,3 +22,12 @@ class TemplatetagsTest(TestCase):
         page = self._create_page(title)
         res = self._render('{{ page|article_title }}', page=page).strip()
         self.assertEqual(res, title)
+
+    @freeze_time('2008-09-02')
+    def test_published_at(self):
+        page = self._create_page('Wild Nacatl')
+        self.assertIsNotNone(page.creation_date)
+        res = self._render(
+            '{{ page|published_at|date:"Y-m-d" }}',
+            page=page).strip()
+        self.assertEqual(res, '2008-09-02')

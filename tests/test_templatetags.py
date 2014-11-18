1245f8a4205e5da6c4dcceb6d591077bb12f7d8e
Anton Egorov <anton.egoroff@gmail.com>
2014-11-19 02:16:22 +0300

    use publication_date for published_at templatetag

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from cms.api import create_page
 from django.template import Template, Context
 from django.test import TestCase
@@ -26,8 +27,14 @@ class TemplatetagsTest(TestCase):
     @freeze_time('2008-09-02')
     def test_published_at(self):
         page = self._create_page('Wild Nacatl')
+        template = '{{ page|published_at|date:"Y-m-d" }}'
+
         self.assertIsNotNone(page.creation_date)
-        res = self._render(
-            '{{ page|published_at|date:"Y-m-d" }}',
-            page=page).strip()
+        self.assertIsNone(page.publication_date)
+        res = self._render(template, page=page).strip()
         self.assertEqual(res, '2008-09-02')
+
+        page.publication_date = page.creation_date + dt.timedelta(days=1)
+        page.save()
+        res = self._render(template, page=page).strip()
+        self.assertEqual(res, '2008-09-03')

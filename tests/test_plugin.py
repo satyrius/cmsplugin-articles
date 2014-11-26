c8a5bd28d696c539f6b1b057e2028b14e109c56a
Anton Egorov <anton.egoroff@gmail.com>
2014-11-26 17:56:57 +0300

    test articles per page limit

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -21,20 +21,35 @@ class PluginTest(CMSTestCase):
         add_plugin(placeholder, 'ArticlesPlugin', 'en', limit=limit)
         return page, placeholder
 
+    def _render(self, placeholder):
+        request = self.get_request(
+            language='en',
+            page=placeholder.page.reload())
+        request_context = RequestContext(request)
+        html = placeholder.render(request_context, None)
+        return BeautifulSoup(html, 'html.parser')
+
     def test_articles(self):
         blog1, body = self._create_blog('Blog 1')
         self._create_page('Article 1', parent=blog1)
         self._create_page('Article 2', parent=blog1)
-        blog1 = blog1.reload()
-        self.assertEqual(len(blog1.get_children()), 2)
+        self.assertEqual(len(blog1.reload().get_children()), 2)
 
         blog2, _ = self._create_blog('Blog 2')
         self._create_page('Article 3', parent=blog2)
         self.assertEqual(len(blog2.reload().get_children()), 1)
 
-        request = self.get_request(language='en', page=blog1)
-        request_context = RequestContext(request)
-        html = body.render(request_context, None)
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = self._render(body)
         titles = {h.string for h in soup.find_all('h2')}
         self.assertEqual(titles, {'Article 1', 'Article 2'})
+
+    def test_page_limit(self):
+        blog1, body = self._create_blog('Blog 1', limit=2)
+        self._create_page('Article 1', parent=blog1)
+        self._create_page('Article 2', parent=blog1)
+        self._create_page('Article 3', parent=blog1)
+
+        soup = self._render(body)
+        titles = {h.string for h in soup.find_all('h2')}
+        # Assertt two latest articles on the first page
+        self.assertEqual(titles, {'Article 2', 'Article 3'})
